c3f220d725f7ae6a893b15fa2ed2d3cc25e338cb
ECRUZY <112583154+ECRUZY@users.noreply.github.com>
2026-05-31 20:04:31 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const app = express()
 
 // ── MIDDLEWARE ──
 app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: '10mb' }))
+app.use(express.urlencoded({ extended: true, limit: '10mb' }))
 
 // ── СТАТИЧЕСКИЕ ФАЙЛЫ (твои HTML) ──
 app.use(express.static(path.join(__dirname, 'public')))
